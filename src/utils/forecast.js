1db1cf458570378ceb4a5c221ac12100cbf49648
Tamizhvendan S <tamizh88@gmail.com>
2021-01-17 19:29:44 +0530

    add formatting

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,15 +1,31 @@
-const request = require('request')
-const forecast=(latitude,longitude,callback)=>{
-    const url='http://api.weatherstack.com/current?access_key=' + process.env.WEATHER_APP_WEATHER_STACK_ACCESS_KEY + '&query=' + latitude +','+ longitude +'&units=m'
-    request({url,json:true},(error,{body})=>{
-        if(error){
-             callback("unable to connect weather",undefined)
-        }else if(body.error){
-            callback("unable to find location.",undefined)
-         }else{
-             
-            callback(undefined,body.current.weather_descriptions[0]+'. it is currently ' +body.current.temperature + ' degree celsius. Winds speed '+ body.current.wind_speed +" km/hr.The Humidity is "+body.current.humidity+"%.")
-      }
-    })
-}
-module.exports=forecast
+const request = require("request");
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    "http://api.weatherstack.com/current?access_key=" +
+        process.env.WEATHER_APP_WEATHER_STACK_ACCESS_KEY +
+        "&query=" +
+        latitude +
+        "," +
+        longitude +
+        "&units=m";
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("unable to connect weather", undefined);
+    } else if (body.error) {
+      callback("unable to find location.", undefined);
+    } else {
+      callback(
+        undefined,
+        body.current.weather_descriptions[0] +
+          ". it is currently " +
+          body.current.temperature +
+          " degree celsius. Winds speed " +
+          body.current.wind_speed +
+          " km/hr.The Humidity is " +
+          body.current.humidity +
+          "%."
+      );
+    }
+  });
+};
+module.exports = forecast;
