5dfbb93b6c7de4a2e21344168c9ffdcc2d98c80d
Tamizhvendan S <tamizh88@gmail.com>
2021-01-17 19:25:49 +0530

    add environment variables for access tokens

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,6 +1,6 @@
 const request = require('request')
 const forecast=(latitude,longitude,callback)=>{
-    const url='http://api.weatherstack.com/current?access_key=useyouraccesskey&query=' + latitude +','+ longitude +'&units=m'
+    const url='http://api.weatherstack.com/current?access_key=' + process.env.WEATHER_APP_WEATHER_STACK_ACCESS_KEY + '&query=' + latitude +','+ longitude +'&units=m'
     request({url,json:true},(error,{body})=>{
         if(error){
              callback("unable to connect weather",undefined)
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address,callback)=>{
-    const url='https://api.mapbox.com/geocoding/v5/mapbox.places/'+address+'.json?access_token=pk.eyJ1IjoidmFzYW50aDcwODUiLCJhIjoiY2tmYzBiMm9tMWI1YzJzcXMyczhmM3N5byJ9.9u_EFd9PMvVAD05y8PVL5g&limit=1'
+  const url='https://api.mapbox.com/geocoding/v5/mapbox.places/'+address+'.json?access_token=' + process.env.WEATHER_APP_MAP_BOX_ACCESS_TOKEN  + '&limit=1'
    request({url,json:true},(error,{body})=>{
        if(error){
             callback("unable to connect location services",undefined)
